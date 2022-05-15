138cba9a07ec1881325fa09115b8a7fbe2b32646
RedSnail <oleg.demianchenko@gmail.com>
2022-05-15 04:28:13 +0400

    fixed imports

--- app/crud.py
+++ app/crud.py
@@ -1,5 +1,5 @@
-import models
-import scemas
+from .models import Node, Edge, Proof
+from .scemas import NodeIn, EdgeIn
 from typing import List, Tuple
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.dialects import postgresql
@@ -14,18 +14,18 @@ class DBOperator(ABC):
 
 
 class NodeOperations(DBOperator):
-    async def add(self, nodes: List[scemas.NodeIn]):
-        db_nodes = list(map(lambda node: models.Node(**node.dict()), nodes))
+    async def add(self, nodes: List[NodeIn]):
+        db_nodes = list(map(lambda node: Node(**node.dict()), nodes))
         self.db_session.add_all(db_nodes)
         await self.db_session.commit()
         return map(lambda node: node.id, db_nodes)
 
 
 class EdgeOperations(DBOperator):
-    async def add(self, edges: List[scemas.EdgeIn]):
+    async def add(self, edges: List[EdgeIn]):
         proofs, edges = zip(*list(map(lambda edge: (
-            models.Proof(date=edge.date_proof, proof=edge.proof, start_id=edge.start_id, end_id=edge.end_id),
-            models.Edge(edge_type=edge.edge_type, start_id=edge.start_id, end_id=edge.end_id)), edges)))
+            Proof(date=edge.date_proof, proof=edge.proof, start_id=edge.start_id, end_id=edge.end_id),
+            Edge(edge_type=edge.edge_type, start_id=edge.start_id, end_id=edge.end_id)), edges)))
 
         [await self.db_session.merge(edge) for edge in edges]
         clust = text("CLUSTER edges USING direction;")
--- database.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-from sqlalchemy.ext.declarative import declarative_base
-
-engine = create_async_engine("postgresql+asyncpg://developer:testpassword@localhost/test.db", future=True, echo=True)
-SessionLocal = sessionmaker(bind=engine, expire_on_commit=False, class_=AsyncSession)
-Base = declarative_base()
